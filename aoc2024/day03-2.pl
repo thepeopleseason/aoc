d5d7bce98e358a9c86319b2d0ba48626f281938a
James Hsiao <matchstick@gmail.com>
2024-12-03 02:20:43 -0500

    further cleanups

--- aoc2024/day03-2.pl
+++ aoc2024/day03-2.pl
@@ -1,18 +1,10 @@
-#!/usr/bin/perl -nl -0777 -F/don\'t\(\)/
+#!/usr/bin/perl -l -0777 -F/don\'t\(\)/
 
 END {
   foreach (@F) {
-    $sum += process_mult_array(
-      [split(/mul/, $_ eq $F[0] ? $_ : [split(m/do\(\)/, $_, 2)]->[1] )]);
+    foreach (split(/mul/, $_ eq $F[0] ? $_ : [split(m/do\(\)/, $_, 2)]->[1] )) {
+      $sum += $2 * $3 if (m/^(\((\d{1,3}),(\d{1,3})\))/);
+    }
   }
   print $sum;
-}
-
-sub process_mult_array {
-  my ($list) = @_;
-  my $sum = 0;
-  foreach (@$list) {
-    $sum += $2 * $3 if (m/^(\((\d{1,3}),(\d{1,3})\))/);
-  }
-  return $sum;
 }
