832cf7122f500dd03d4c32fd243c34db75f6707b
James Hsiao <matchstick@gmail.com>
2024-12-03 01:03:50 -0500

    updated

---
+++ aoc2024/day03-2.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl -nl -F/don\'t\(\)/
+
+END {
+  for my $token (@F) {
+    if ($token eq $F[0]) {
+      $sum += process_mult_array([split(/mul/, $token)]);
+    }
+    else {
+      next unless $token =~ m/do\(\)/;
+      my @dos = split(m/do\(\)/, $token, 2);
+      my @muls = split(/mul/,$dos[1]);
+      $sum += process_mult_array(\@muls);
+    }
+  }
+  print $sum;
+}
+
+sub process_mult_array {
+  my ($list) = @_;
+  my $sum = 0;
+  foreach (@$list) {
+    $sum += $2 * $3 if (m/^(\((\d{1,3}),(\d{1,3})\))/);
+  }
+  return $sum;
+}
--- aoc2024/day03.pl
+++ aoc2024/day03.pl
@@ -1,16 +1,7 @@
 #!/usr/bin/perl -nl -F/mul/
-
-use Data::Dumper;
-
-BEGIN { my $sum = 0; }
 END {
-  for my $token (@F) {
-    print "$token";
-    if ($token =~ m/^(\((\d{1,3}),(\d{1,3})\))/) {
-      my $mult = $2 * $3;
-      print "\t$1 : $mult";
-      $sum += $mult;
-    }
+  foreach (@F) {
+    $sum += $2 * $3 if (m/^(\((\d{1,3}),(\d{1,3})\))/);
   }
   print $sum;
 }
